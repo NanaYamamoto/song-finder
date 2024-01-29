189128e28132be3fc136a6de6c2a501cd5ea95e6
NanaYamamoto <chyhyy76@gmail.com>
2024-01-29 22:25:35 +0900

    output error

--- script.js
+++ script.js
@@ -2,13 +2,11 @@
 /// Access-Control-Allow-Originエラーを回避する
 
 
-const url = 'https://itunes.apple.com/search?term=beatles'
-fetch(url, {
-    method: 'GET',
-    mode: 'cors',
-})
+const url = 'https://itunes.apple.com/search?term=beatles&limit=3'
+fetch(url)
     .then((Response) => Response.json())
-    .then((date) => {
-        console.log(data);
+    .then((data) => {
+        console.log(data.results);
     })
+    .catch(error => console.log('Request failed:', error))
 
