cbb4c50f89410dbd47132423823cd471ac90743e
NanaYamamoto <chyhyy76@gmail.com>
2024-01-31 21:27:46 +0900

    finish it up

--- script.js
+++ script.js
@@ -1,44 +1,74 @@
 
-/// Access-Control-Allow-Originエラーを回避する
+let term = ''
+const songContainer = document.getElementById('container');
+
+const updateTerm = () => {
+    term = document.getElementById('searchInput').value;
+
+    if (!term || term === '') {
+        alert('Please enter a search term');
+    } else {
+
+        while (songContainer.firstChild) {
+            songContainer.removeChild(songContainer.firstChild);
+        }
+
+        const url = `https://itunes.apple.com/search?term=${term}&media=music&limit=9`
+        fetch(url)
+            .then((Response) => Response.json())
+            .then((data) => {
+                //console.log(data.results);
+                const artists = data.results;
+                //HTMLにElementを作成する
+                return artists.map(result => {
+                    const article = document.createElement('article'),
+                        artist = document.createElement('p'),
+                        song = document.createElement('p'),
+                        image = document.createElement('img'),
+                        audio = document.createElement('audio'),
+                        audioSource = document.createElement('source')
+
+                    //article にidを付与する
+                    article.className = "item";
+                    audio.className = "audio";
+
+                    //HTMLに編集する
+                    artist.innerHTML = result.artistName
+                    song.innerHTML = result.trackName
+                    image.src = result.artworkUrl100
+                    audioSource.src = result.previewUrl
+                    audio.setAttribute('controls', '')
+
+                    //親要素(article)に入れる
+                    article.appendChild(image)
+                    article.appendChild(artist)
+                    article.appendChild(song)
+                    article.appendChild(audio)
+                    //audioSourceをaudio(親要素)の配下に入れる
+                    audio.appendChild(audioSource)
+
+                    //containerの配下に入れる
+                    songContainer.appendChild(article);
+
+                })
+            })
+            .catch(error => console.log('Request failed:', error))
 
 
-const url = 'https://itunes.apple.com/search?term=The=Beatles&artistName=Beatles&limit=6&entity=song'
-fetch(url)
-    .then((Response) => Response.json())
-    .then((data) => {
-        //console.log(data.results);
-        const artists = data.results;
-        //HTMLにElementを作成する
-        return artists.map(result => {
+    }
+}
 
-            const songContainer = document.getElementById('container');
-            const article = document.createElement('article'),
-                artist = document.createElement('p'),
-                song = document.createElement('p'),
-                image = document.createElement('img'),
-                audio = document.createElement('audio'),
-                audioSource = document.createElement('source')
+const searchBtn = document.querySelector('button');
+searchBtn.addEventListener('click', updateTerm);
 
-            //HTMLに編集する
-            artist.innerHTML = result.artistName
-            song.innerHTML = result.trackName
-            image.src = result.artworkUrl100
-            audioSource.src = result.previewUrl
-            audio.setAttribute('controls', '')
+document.addEventListener('play', event => {
+    const audio = document.getElementsByTagName('audio');
+    for (i = 0; i < audio.length; i++) {
+        if (audio[i] != event.target) {
+            audio[i].pause();
+            console.log(event);
+        }
+    }
+}, true)
 
-            //親要素(article)に入れる
-            article.appendChild(image)
-            article.appendChild(artist)
-            article.appendChild(song)
-            article.appendChild(audio)
-            //audioSourceをaudio(親要素)の配下に入れる
-            audio.appendChild(audioSource)
-
-            //containerの配下に入れる
-            songContainer.appendChild(article);
-
-            console.log(result);
-        })
-    })
-    .catch(error => console.log('Request failed:', error))
 
